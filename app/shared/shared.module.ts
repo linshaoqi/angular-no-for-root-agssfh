109263b8041913dc58974a45158bd99a0aaecefa
linshaoqi <13922168603@139.com>
2023-05-04 17:52:22 +0800

    Initial commit

---
+++ app/shared/shared.module.ts
@@ -0,0 +1,12 @@
+import { ModuleWithProviders, NgModule } from '@angular/core';
+import { CounterService } from './counter.service';
+
+@NgModule({})
+export class SharedModule {
+  static forRoot(): ModuleWithProviders {
+    return {
+      ngModule: SharedModule,
+      providers: [ CounterService ]
+    }
+  }
+}
